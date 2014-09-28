df565c13c0b2d92f8b3d34328512bac3b19474d4
James Robinson <github@epii.co.uk>
2014-09-28 23:37:18 +0100

    getting within must be inequality at maximum to allow to be split up into smaller rectanges more easily

--- src/main/java/uk/co/epii/politics/williamcavendishbentinck/DatabaseSessionImpl.java
+++ src/main/java/uk/co/epii/politics/williamcavendishbentinck/DatabaseSessionImpl.java
@@ -28,8 +28,8 @@ public class DatabaseSessionImpl implements DatabaseSession {
             "SELECT %1$s.* FROM %2$s a INNER JOIN %3$s b ON a.%4$s = b.%4$s INNER JOIN " +
                     "Classification c ON a.UPRN = c.UPRN " +
             "WHERE (c.UPRN IS NULL OR SUBSTR(c.CLASSIFICATION_CODE,1,1) = 'R') AND " +
-                    "a.X_COORDINATE >= :minX AND a.X_COORDINATE <= :maxX AND " +
-                    "a.Y_COORDINATE >= :minY AND a.Y_COORDINATE <= :maxY ORDER BY a.%5$s";
+                    "a.X_COORDINATE >= :minX AND a.X_COORDINATE < :maxX AND " +
+                    "a.Y_COORDINATE >= :minY AND a.Y_COORDINATE < :maxY ORDER BY a.%5$s";
 
     private String rawGetByIdSql = "SELECT * FROM %1$s WHERE ID IN (%2$s)";
 
